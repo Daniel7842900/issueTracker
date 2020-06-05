aa18e52c7d573c6fb2e07a9fb77d9339d1f1f582
Daniel Lim <daniellim@Daniels-MacBook-Pro.local>
2020-06-05 16:38:24 -0700

    successfully implemented store function

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use App\Ticket;
 
 
 class TicketController extends Controller
@@ -28,7 +30,20 @@ class TicketController extends Controller
     public function store(Request $request) {
         $ticket = new Ticket();
 
-        dd($request->input());
-        $ticket->name = request('ticket_name');
+        //dd($request->input());
+        $ticket->title = request('ticket_title');
+        $ticket->description = request('ticket_desc');
+        $ticket->project_id = request('ticket_project');
+        $ticket->type = request('ticket_type');
+        $ticket->priority = request('ticket_priority');
+
+        $user_id = Auth::id();
+        //dd($user_id);
+
+        $ticket->submitter_id = $user_id;
+        $ticket->status = 'open';
+        $ticket->save();
+
+        return redirect('/ticket');
     }
 }
--- resources/views/ticket/create.blade.php
+++ resources/views/ticket/create.blade.php
@@ -5,18 +5,18 @@
     <div class="row">
         <div class="col-md-12">
             <h1>Create a New Ticket</h1>
-            <form action="/project" method="POST">
+            <form action="/ticket" method="POST">
                 @csrf
-                <label for="ticket_name">Ticket title:</label>
-                <input type="text" id="ticket_name" name="ticket_name">
+                <label for="ticket_title">Ticket title:</label>
+                <input type="text" id="ticket_title" name="ticket_title">
                 <label for="ticket_desc">Ticket description:</label>
                 <input type="text" id="ticket_desc" name="ticket_desc">
                 <label for="ticket_project">Choose Project:</label>
-                @foreach($projects as $project)
                 <select name="ticket_project" id="ticket_project">
-                    <option value="">{{ $project->title }}</option>
+                    @foreach($projects as $project)
+                    <option value="{{ $project->id }}">{{ $project->title }}</option>
+                    @endforeach
                 </select>
-                @endforeach
                 <label for="ticket_type">Choose Type:</label>
                 <select name="ticket_type" id="ticket_type">
                     <option value="bug">Bug/Error</option>
